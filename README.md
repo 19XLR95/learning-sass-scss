83b7a5ab93dc41fa95692a7c7f103c3b2b45883e
19XLR95 <omer.aydin.business@gmail.com>
2020-03-13 22:21:09 +0300

    files uploaded

---
+++ README.md
@@ -0,0 +1,4 @@
+# learning scss/sass
+
+learning scss/sass with official docs
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.scss";
+
+(
+    () => {
+        let x = 5 * 5;
+        console.log("RESULT: " + (x + 25));
+    }
+)();
